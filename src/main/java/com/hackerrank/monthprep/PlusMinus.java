c51cec6203b783764c7836d91e6f6abef7c5d841
Hafiz-G <30983575+Hafiz-G@users.noreply.github.com>
2022-10-18 01:26:40 +0530

    Solve PlusMinus

---
+++ src/main/java/com/hackerrank/monthprep/PlusMinus.java
@@ -0,0 +1,48 @@
+package com.hackerrank.monthprep;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class PlusMinus {
+    public static void plusMinus(List<Integer> arr) {
+        int positive = 0, negative = 0, zero = 0;
+
+        for (Integer i : arr) {
+            if (i > 0) {
+                positive++;
+            } else if (i < 0) {
+                negative++;
+            } else {
+                zero++;
+            }
+        }
+        DecimalFormat decimalFormat = new DecimalFormat("0.000000");
+
+        System.out.println(decimalFormat.format((double) positive / arr.size()));
+        System.out.println(decimalFormat.format((double) negative / arr.size()));
+        System.out.println(decimalFormat.format((double) zero / arr.size()));
+    }
+
+}
+
+class Runner {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" ")).map(Integer::parseInt).collect(toList());
+
+        PlusMinus.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
+
+
---
+++ src/test/java/com/hackerrank/monthprep/PlusMinusTest.java
@@ -0,0 +1,47 @@
+package com.hackerrank.monthprep;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlusMinusTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testPlusMinus1() {
+        PlusMinus.plusMinus(List.of(-4, 3, -9, 0, 4, 1));
+        assertEquals("""
+                0.500000\r
+                0.333333\r
+                0.166667""", outContent.toString().trim());
+    }
+
+    @Test
+    public void testPlusMinus2() {
+        PlusMinus.plusMinus(List.of(1, 2, 3, -1, -2, -3, 0, 0));
+        assertEquals("""
+                0.375000\r
+                0.375000\r
+                0.250000""", outContent.toString().trim());
+    }
+
+}
