ed8d980ccab8498516b39bd8e09a9fe184e69a68
Hafiz-G <30983575+Hafiz-G@users.noreply.github.com>
2022-10-19 01:11:35 +0530

    Solve Sparse Arrays

---
+++ src/main/java/com/hackerrank/monthprep/SparseArrays.java
@@ -0,0 +1,69 @@
+package com.hackerrank.monthprep;
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class SparseArrays {
+
+
+    public static List<Integer> matchingStrings(List<String> strings, List<String> queries) {
+        Map<String, Integer> counts = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (String str : strings) {
+            if (counts.containsKey(str)) {
+                counts.put(str, counts.get(str) + 1);
+            } else {
+                counts.put(str, 1);
+            }
+        }
+
+        for (String query : queries) {
+            result.add(counts.getOrDefault(query, 0));
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int stringsCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> strings = IntStream.range(0, stringsCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }).collect(toList());
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> queries = IntStream.range(0, queriesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }).collect(toList());
+
+        List<Integer> res = matchingStrings(strings, queries);
+
+        bufferedWriter.write(res.stream().map(Object::toString).collect(joining("\n")) + "\n");
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+}
+
---
+++ src/test/java/com/hackerrank/monthprep/SparseArraysTest.java
@@ -0,0 +1,19 @@
+package com.hackerrank.monthprep;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SparseArraysTest {
+    @Test
+    public void testSparseArray1() {
+        List<String> strings = List.of("aba", "baba", "aba", "xzxb");
+        List<String> queries = List.of("aba", "xzxb", "ab");
+
+        List<Integer> result = SparseArrays.matchingStrings(strings, queries);
+
+        assertEquals(List.of(2, 1, 0), result);
+    }
+}
