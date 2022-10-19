a4352c5bce9288a06c93aadcf5203c506991be00
Hafiz-G <30983575+Hafiz-G@users.noreply.github.com>
2022-10-19 23:12:55 +0530

    Solve Counting sort

---
+++ src/main/java/com/hackerrank/monthprep/CountingSort.java
@@ -0,0 +1,15 @@
+package com.hackerrank.monthprep;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CountingSort {
+    public static List<Integer> countingSort(List<Integer> arr) {
+        List<Integer> list = new ArrayList<>(Collections.nCopies(100, 0));
+        for (Integer n : arr) {
+            list.set(n, list.get(n) + 1);
+        }
+        return list;
+    }
+}
