c0c87a4ae92b7c504856ad295eda374355cee006
Hafiz-G <30983575+Hafiz-G@users.noreply.github.com>
2022-10-19 23:30:10 +0530

    Solve Pangram

---
+++ src/main/java/com/hackerrank/monthprep/Pangram.java
@@ -0,0 +1,19 @@
+package com.hackerrank.monthprep;
+
+import java.util.Arrays;
+
+public class Pangram {
+    public static String pangrams(String s) {
+        boolean[] arr = new boolean[26];
+        Arrays.fill(arr, false);
+        for (char c : s.toLowerCase().toCharArray()) {
+            if (c != ' ') {
+                arr[c - 'a'] = true;
+            }
+        }
+        for (boolean b : arr) {
+            if (!b) return "not pangram";
+        }
+        return "pangram";
+    }
+}
---
+++ src/test/java/com/hackerrank/monthprep/PangramTest.java
@@ -0,0 +1,14 @@
+package com.hackerrank.monthprep;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PangramTest {
+
+    @Test
+    public void testPangram() {
+        assertEquals("pangram", Pangram.pangrams("We promptly judged antique ivory buckles for the next prize"));
+        assertEquals("not pangram", Pangram.pangrams("We promptly judged antique ivory buckles for the prize"));
+    }
+}
