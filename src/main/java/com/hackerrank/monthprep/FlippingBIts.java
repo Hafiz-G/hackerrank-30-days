25a33ab6e52cc8408500904b4ac1fc49deabd544
Hafiz-G <30983575+Hafiz-G@users.noreply.github.com>
2022-10-19 22:13:40 +0530

    Solve Flipping Bits

---
+++ src/main/java/com/hackerrank/monthprep/FlippingBIts.java
@@ -0,0 +1,7 @@
+package com.hackerrank.monthprep;
+
+public class FlippingBIts {
+    public static long flippingBits(long n) {
+        return (long)Math.pow(2, 32) -1 ^ n;
+    }
+}
---
+++ src/test/java/com/hackerrank/monthprep/FlippingBItsTest.java
@@ -0,0 +1,16 @@
+package com.hackerrank.monthprep;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlippingBItsTest {
+
+    @Test
+    public  void testFlippingBits1(){
+        assertEquals(2147483648L, FlippingBIts.flippingBits(2147483647));
+        assertEquals(4294967294L, FlippingBIts.flippingBits(1));
+        assertEquals(4294967295L, FlippingBIts.flippingBits(0));
+    }
+
+}
